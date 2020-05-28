a65101cd6be827bc74726a6db0d479516832197b
Naggas07 <rober.araya.gil@gmail.com>
2020-05-28 12:22:53 +0200

    added read folders

--- process/unifBas.ts
+++ process/unifBas.ts
@@ -29,17 +29,72 @@ const dayOfYear = (year: any, month: number, day: number) => {
   return days;
 };
 
-console.log(dayOfYear(2019, 3, 15));
-console.log(date.getDay());
+console.log("Dia del año", dayOfYear(2019, 3, 15));
+console.log("Dia de la semana", date.getDay());
 
-if (date.getDay() === 3) {
+const dosCaracteres = (number: number) => {
+  return number < 10 ? "0" + number : number;
+};
+
+const diaCarpeta = (year: number, day: number) => {
+  const fecha = new Date(year, 0, day);
+
+  return `${dosCaracteres(fecha.getDate())}-${dosCaracteres(
+    fecha.getMonth() + 1
+  )}-${year}`;
+};
+
+const diaFichero = (year: number, day: number) => {
+  const fecha = new Date(year, 0, day);
+
+  return `${year}${dosCaracteres(fecha.getMonth() + 1)}${dosCaracteres(
+    fecha.getDate()
+  )}`;
+};
+
+const yearDate = dayOfYear(date.getFullYear(), date.getMonth(), date.getDate());
+
+if (date.getDay() === 4) {
   //creamos fichero acumulado
-  await files.createFile(normalize.archiveName.acumBascula, "txt");
+  // await files.createFile(normalize.archiveName.acumBascula, "txt");
 
-  let header = await files.cabeceraNifContrato();
-  await Deno.writeFile(normalize.routes.basculaSemanal, header, {
-    append: true,
-  });
+  // let header = await files.cabeceraNifContrato();
+  // await Deno.writeFile(normalize.routes.basculaSemanal, header, {
+  //   append: true,
+  // });
+
+  let output = await Deno.readDir("./output/");
+  let folders = [];
+  for await (const folder of output) {
+    for (let i = 0; i < 7; i++) {
+      if (yearDate - i < 0) {
+        let lastYearDate =
+          date.getFullYear() % 4 === 0
+            ? 366 - (yearDate - i)
+            : 365 - (yearDate - i);
+
+        if (folder.name === diaCarpeta(date.getFullYear() - 1, lastYearDate)) {
+          folders.push(folder.name);
+        }
+      } else {
+        if (folder.name === diaCarpeta(date.getFullYear(), yearDate - i)) {
+          folders.push(folder.name);
+        }
+      }
+    }
+  }
+
+  console.log(folders);
+
+  let dato = folders
+    .map((item: any) => `./output/${item}/`)
+    .map((item: any) => {
+      for await (const dirEntry of Deno.readDir(item)) {
+        return dirEntry.name;
+      }
+    });
+
+  console.log("DIOSOOSOS", dato);
 
   let archivo = await Deno.readFile("./output/27-05-2020/ERRORES_20200527.csv");
   let text = new TextDecoder().decode(archivo).trim().split("\n");
@@ -57,5 +112,16 @@ if (date.getDay() === 3) {
 
   console.log(arr);
 
+  let archive = [];
   for (let i = 0; i < 7; i++) {}
+
+  console.log(await Deno.readDir("./output/26-05-2020/"));
+  for await (const dirEntry of Deno.readDir("./output/26-05-2020/")) {
+    console.log(dirEntry);
+    if (dirEntry.name.includes("COMODIN")) {
+      archive.push(`./output/26-05-2020/COMODIN_20200526.csv`);
+    }
+  }
+
+  console.log(archive);
 }
